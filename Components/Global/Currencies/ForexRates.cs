82f2e3dfacae848da326a93d78d3506a8ecb7b2e
Daniel Lozano <d.lozanonavas@outlook.com>
2021-03-17 00:34:40 -0500

    Refactor: Forex Object

---
+++ Components/Global/Currencies/ForexRates.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace FenixAlliance.ABM.Models.DTOs.Components.Global.Currencies
+{
+    public class ForexRates
+    {
+        [JsonProperty("timestamp")]
+        public long Timestamp { get; set; }
+
+        [JsonProperty("base")]
+        public string Base { get; set; }
+
+        [JsonProperty("rates")]
+        public Dictionary<string, double> Rates { get; set; } = new Dictionary<string, double>();
+    }
+}
